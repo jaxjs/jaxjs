2cd1be6c972517bbf9e3febdfa0da56128b80641
Nick Sagona <nick@moc10media.com>
2014-11-28 09:53:30 -0600

    Start working on the tests

---
+++ tests/ajax/process/ajax.php
@@ -0,0 +1,5 @@
+<?php
+    $username = $_POST['user_name'];
+    $email = $_POST['user_email'];
+    echo 'Your Username: ' . $username . '<br />' . PHP_EOL . 'Your Email: ' . $email . '<br />' . PHP_EOL;
+?>
